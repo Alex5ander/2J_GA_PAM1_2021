d3a85aa1276fd09dbe55d3e6fc008774038a4009
Alexsander Gutierrez Gonçalves <alexsandergutierreez@gmail.com>
2021-08-19 23:45:21 +0000

    Aula 4

--- ion1trim1/src/app/home/home.page.ts
+++ ion1trim1/src/app/home/home.page.ts
@@ -6,18 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  login: string = "";
-  senha: string = "";
-  mensagem: string = "";
-  
-  validar() :void{
-    if(this.login === "admin" && this.senha === "1234"){
-      this.mensagem = "Usuário logado";
-    }else {
-      this.mensagem = "Falha na autenticação";
-    }
-  }
+  num1 = null;
+  num2 = null;
+  resposta = '';
   
   constructor() {}
 
+  temnumero(): boolean {
+    return this.num1 !== null && this.num2 !== null;
+  }
+
+  somar(): void {
+    if(this.temnumero()){
+      this.resposta = String(this.num1 + this.num2);
+    }
+  }
+
+  subtrair(): void {
+    if(this.temnumero()){
+      this.resposta = String(this.num1 - this.num2);
+    }
+  }
+
+  multiplicar(): void {
+    if(this.temnumero()){
+      this.resposta = String(this.num1 * this.num2);
+    }
+  }
+
+  dividir(): void {
+    if(this.temnumero()){
+      if(this.num2 === 0){
+        this.resposta = "Não pode ser dividido por zero";
+      }else {
+        this.resposta = String(this.num1 / this.num2);
+      }
+    }
+  }
 }
