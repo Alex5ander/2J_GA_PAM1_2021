5306528dae2c29bf7a274af0d6f605e684d5432b
Alex5ander <alexsandergutierreez@gmail.com>
2021-08-30 20:40:43 -0300

    Atividade Aplicativo Qual o combustível

--- qualocombustivel/src/app/home/home.page.ts
+++ qualocombustivel/src/app/home/home.page.ts
@@ -8,10 +8,25 @@ import { Component } from '@angular/core';
 export class HomePage {
   gasolina = null;
   etanol = null;
-  imagem = "";
+  images = ["", ""];
+  title = "";
   constructor() {}
 
   verificar(): void{
-
+   if(this.gasolina != null && this.etanol != null){
+    let g = this.etanol + (this.etanol * .3);
+    this.title = "";
+    this.images = ["", ""];
+    if(g > this.gasolina){
+      this.title = "Gasolina";
+      this.images[0] = "gasolina.png";
+    }else if(g < this.gasolina){
+      this.title = "Etanol";
+      this.images[1] = "etanol.png";
+    }else{
+      this.title = "Gasolina e Etanol";
+      this.images = ["gasolina.png", "etanol.png"];
+    }
+   }
   }
 }
