1e7fb87a463cd5459eb3ff7e693cbe525d136272
Alex5ander <alexsandergutierreez@gmail.com>
2021-09-14 20:47:27 -0300

    Aula 7.1

--- qualocombustivel/src/app/home/home.page.ts
+++ qualocombustivel/src/app/home/home.page.ts
@@ -15,12 +15,14 @@ export class HomePage {
   verificar(): void{
    if(this.gasolina != null && this.etanol != null){
     let diff = this.gasolina - this.etanol;
+    let p3 = parseFloat((this.gasolina * .3).toFixed(2));
     this.title = null
     this.images = [null, null];
-    if(diff < this.gasolina * .3){
+
+    if(diff < p3){
       this.title = "Gasolina";
       this.images[0] = "gasolina.png";
-    }else if(diff > this.gasolina * .3){
+    }else if(diff > p3){
       this.title = "Etanol";
       this.images[1] = "etanol.png";
     }else{
