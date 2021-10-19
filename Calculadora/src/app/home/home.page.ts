4a9af760072c8412126ea427e480908e6c4de595
Alex5ander <alexsandergutierreez@gmail.com>
2021-10-19 21:43:50 -0200

    Aula 19/10/2021

--- Calculadora/src/app/home/home.page.ts
+++ Calculadora/src/app/home/home.page.ts
@@ -10,36 +10,45 @@ export class HomePage {
   valor = '';
   n1 = '';
   n2 = '';
-  operador = null;
+  operador = '';
 
   constructor() {}
 
   insereUmDigito(digito) {
     let ultimoDigito = this.valor[this.valor.length -1];
-    if(this.eNumero(digito)){
-      if(!this.operador){
-          this.n1 += digito;
-          this.valor += digito;
-      }else {
-        this.n2 += digito;
-        this.valor += digito;
+    if(digito === '.'){
+      if(this.n1 === '') {
+        this.n1 = '0.'
+      }else if(this.n1.search(/\./) === -1) {
+        this.n1 += '.'
       }
-    }else{
-      if(this.eNumero(ultimoDigito)){
-        if(!this.operador){
-          this.operador = digito;
-          this.valor += digito;
-        }else {
-          this.calcular();
-          this.operador = digito;
-          this.valor += this.operador;
+      if(this.operador) {
+        if(this.n2 === '') {
+          this.n2 = '0.'
+        }else if(this.n2.search(/\./) === -1) {
+          this.n2 += '.'
         }
       }
+    }else if(this.eNumero(digito)){
+      if(!this.operador){
+          this.n1 += digito;
+      }else {
+        this.n2 += digito;
+      }
+    }else if(this.eNumero(ultimoDigito)){
+      if(!this.operador){
+        this.operador = digito;
+      }else {
+        this.calcular();
+        this.operador = digito;
+      }
     }
+    this.valor = this.n1+''+this.operador+''+this.n2;
   }
   eNumero(digito) {
     return !isNaN(digito);
   }
+  eOperador(digito) {return digito === "+" || digito === '-' || digito === 'x' || digito === '/'}
   calcular() {
     if(this.n1 && this.n2 && this.operador){
       if(this.operador === '+'){
@@ -51,6 +60,7 @@ export class HomePage {
       }else if(this.operador === '/'){
         this.valor = (parseFloat(this.n1) / parseFloat(this.n2)).toString();
       }
+
       this.n1 = this.valor;
       this.n2 = '';
       this.operador = '';
