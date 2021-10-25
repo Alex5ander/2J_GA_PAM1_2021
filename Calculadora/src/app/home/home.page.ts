863838b19529a7165cc30424f462830ba037c9af
Alex5ander <alexsandergutierreez@gmail.com>
2021-10-25 21:41:48 -0200

    Aula 25/10/2021

--- Calculadora/src/app/home/home.page.ts
+++ Calculadora/src/app/home/home.page.ts
@@ -14,51 +14,48 @@ export class HomePage {
 
   constructor() {}
 
-  insereUmDigito(digito) {
-    let ultimoDigito = this.valor[this.valor.length -1];
-    if(digito === '.'){
-      if(this.n1 === '') {
-        this.n1 = '0.'
-      }else if(this.n1.search(/\./) === -1) {
-        this.n1 += '.'
-      }
-      if(this.operador) {
-        if(this.n2 === '') {
-          this.n2 = '0.'
-        }else if(this.n2.search(/\./) === -1) {
-          this.n2 += '.'
-        }
-      }
-    }else if(this.eNumero(digito)){
+  insereUmDigito(digito: string) {
+    if(this.eNumero(digito)){
       if(!this.operador){
-          this.n1 += digito;
+        this.n1 += digito;
       }else {
         this.n2 += digito;
       }
-    }else if(this.eNumero(ultimoDigito)){
-      if(!this.operador){
+    }else if(this.eOperador(digito)) {
+      if(this.n1 !== "" && this.n2 === "") {
         this.operador = digito;
-      }else {
+      }else if(this.n2 !== "") {
         this.calcular();
         this.operador = digito;
       }
+    }else if(digito === ".") {
+      if(this.n1 === "") {
+        this.n1 = "0.";
+      }else if(this.operador === "" && this.n1.search(/\./) == -1){
+       this.n1 += digito;
+      }else if(this.operador !== "") {
+        if(this.n2 === ""){
+          this.n2 = "0.";
+        }else if(this.n2.search(/\./) == -1){
+          this.n2 += digito;
+        }
+      }
     }
+
     this.valor = this.n1+''+this.operador+''+this.n2;
   }
-  eNumero(digito) {
-    return !isNaN(digito);
-  }
-  eOperador(digito) {return digito === "+" || digito === '-' || digito === 'x' || digito === '/'}
   calcular() {
-    if(this.n1 && this.n2 && this.operador){
+    if(parseFloat(this.n2) === 0) {
+      alert("Erro!\nNão é possível dividir por 0");
+    }else if(this.n1 && this.n2 && this.operador){
       if(this.operador === '+'){
-        this.valor = (parseFloat(this.n1) + parseFloat(this.n2)).toString();
+        this.valor = this.somar();
       }else if(this.operador === '-'){
-        this.valor = (parseFloat(this.n1) - parseFloat(this.n2)).toString();
+        this.valor = this.subtrair();
       }else if(this.operador === 'x'){
-        this.valor = (parseFloat(this.n1) * parseFloat(this.n2)).toString();
+        this.valor = this.multiplicar();
       }else if(this.operador === '/'){
-        this.valor = (parseFloat(this.n1) / parseFloat(this.n2)).toString();
+        this.valor = this.dividir();
       }
 
       this.n1 = this.valor;
@@ -66,6 +63,12 @@ export class HomePage {
       this.operador = '';
     }
   }
+  eNumero(digito) { return !isNaN(digito); }
+  eOperador(digito) { return digito === "+" || digito === '-' || digito === 'x' || digito === '/'; }
+  somar() { return (parseFloat(this.n1) + parseFloat(this.n2)).toString() }
+  subtrair() { return (parseFloat(this.n1) - parseFloat(this.n2)).toString() }
+  multiplicar() { return (parseFloat(this.n1) * parseFloat(this.n2)).toString() }
+  dividir() { return (parseFloat(this.n1) / parseFloat(this.n2)).toString() }
   limpar() {
     this.n1 = '';
     this.n2 = '';
